5eb906afff7cb54bfdd9193bf9f8de1a021c6909
mikelagumbay22 <mikelagumbay@hotmail.com>
2025-04-11 11:31:12 -0400

    fix/fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    outputFileTracingRoot: undefined
+  }
 }
 
 module.exports = nextConfig 
